bfac34d51aa627a1d498655c456068a5e968519f
aleenatomy2802 <aleena.tomy@g.austincc.edu>
2025-03-03 20:05:40 -0600

    I made changes

--- GWC/src/App.tsx
+++ GWC/src/App.tsx
@@ -28,6 +28,12 @@ function App() {
             <p>Major: Computer Science</p>
           </div>
 
+          <div className="Member-box">
+            <h3>Aleena Tomy😁</h3>
+            <p>Year: Junior</p>
+            <p>Major: Computer Science</p>
+          </div>
+
         </div>
       </div>
     </div>
